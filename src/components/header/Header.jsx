66b8a6e71190933d7297244888a8fea47f5d7282
nyh <nsi3358@naver.com>
2024-01-12 00:52:19 +0900

    햄버거 메뉴바 import

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HamburgerMenu from './element/HamburgerMenu';
+
+export default function Header() {
+  return (
+    <div>
+      <HamburgerMenu />
+    </div>
+  );
+}
---
+++ src/components/header/element/HamburgerMenu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
+
+export default function HamburgerMenu() {
+  return (
+    <div>
+      <RxHamburgerMenu />
+    </div>
+  );
+}
