20b7e6b4c3de1992b4ef202082a9ca2821b603bd
nyh <nsi3358@naver.com>
2024-01-13 04:24:58 +0900

    customNav prop 타입명 변경

--- src/components/utils/CustomNav.tsx
+++ src/components/utils/CustomNav.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-type CustomNav = {
+type NavProps = {
   children: React.ReactNode;
   title: React.ReactNode;
 };
 
-export default function CustomNav({ children, title }: CustomNav) {
+export default function CustomNav({ children, title }: NavProps) {
   return (
     <div className="flex  h-16 group relative justify-center items-center dark:text-white">
       <button className="flex flex-col justify-center items-center ml-1.5 group-hover:cursor-pointer z-10">
         {children}
         <p className="text-xs">{title}</p>
       </button>
-
       <div className="w-full h-full ml-1.5 rounded-xl absolute group-hover:bg-stone-300 opacity-25"></div>
     </div>
   );
