5e57cbdd6f595555540444dce4706988f377f223
nyh <nsi3358@naver.com>
2024-01-13 01:08:51 +0900

    메인 로고 반응형 기능 추가

--- src/components/side/element/firstNav/children/HomeNav.tsx
+++ src/components/side/element/firstNav/children/HomeNav.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { IoMdHome } from 'react-icons/io';
 import CustomNav from '../../../../utils/CustomNav';
+import { Link } from 'react-router-dom';
 
 export default function HomeNav() {
   return (
     <CustomNav title={'홈'}>
-      <IoMdHome className="w-7 h-7" />
+      <Link to="/">
+        <IoMdHome className="w-7 h-7" />
+      </Link>
     </CustomNav>
   );
 }
