a10c37fe3f6e8b0459cefb64a57aedca3ecaee38
nyh <nsi3358@naver.com>
2024-01-15 03:28:53 +0900

    로딩 페이지 투명도 삭제

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading } from 'react-icons/ai';
 
 export default function LoadingPage() {
   return (
-    <div className="h-full w-full flex justify-center items-center opacity-20">
+    <div className="h-full w-full flex justify-center items-center">
       <AiOutlineLoading className="h-1/4 w-1/5 animate-spin" />
     </div>
   );
