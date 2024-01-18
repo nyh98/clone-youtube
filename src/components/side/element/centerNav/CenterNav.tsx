4c1044daf2bb3418238a8dd0c83888c409c04811
nyh <nsi3358@naver.com>
2024-01-19 02:13:55 +0900

    햄버거 메뉴 클릭시 사이드 메뉴 경계선 지정

--- src/components/side/element/centerNav/CenterNav.tsx
+++ src/components/side/element/centerNav/CenterNav.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CustomNav from '../../../utils/CustomNav';
 import { MdOutlineMoreTime } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import { hamburgerContext } from '../../../../context/HamburgerMenuContext';
 
 export default function CenterNav() {
+  const { hamburgerMenuChecked } = useContext(hamburgerContext);
   return (
-    <Link to={'/watch'} className="hover:cursor-pointer">
+    <Link
+      to={'/watch'}
+      style={hamburgerMenuChecked ? { borderBottomWidth: '1px' } : {}}
+      className="hover:cursor-pointer"
+    >
       <CustomNav title={'시청 기록'}>
         <MdOutlineMoreTime className="w-7 h-7" />
       </CustomNav>
--- src/components/side/element/firstNav/FirstNav.tsx
+++ src/components/side/element/firstNav/FirstNav.tsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
 import HomeNav from './children/HomeNav';
 import SubscribeNav from './children/SubscribeNav';
+import { hamburgerContext } from '../../../../context/HamburgerMenuContext';
 
 export default function FirstNav() {
+  const { hamburgerMenuChecked } = useContext(hamburgerContext);
   return (
-    <div className="flex flex-col">
+    <div
+      style={hamburgerMenuChecked ? { borderBottomWidth: '1px' } : {}}
+      className="flex flex-col"
+    >
       <HomeNav />
       <SubscribeNav />
     </div>
